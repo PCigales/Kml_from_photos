80301bf8d7fb3688b931b9e5d486f1090ddf6ab2
PCigales <58741513+PCigales@users.noreply.github.com>
2022-07-07 19:26:16 +0200

    Improvements

--- kml-andro.bat
+++ kml-andro.bat
@@ -8,29 +8,30 @@ echo.
 echo Scan de "%kmlrepscan%" pour cr�ation des miniatures dans "%kmlrepdest%thumbs\"
 for /F "delims=/" %%i in ("%kmlrepscan:~0,-1%") do set kmlname=%%~ni
 if exist "%kmlrepdest%%kmlname%.lst" del "%kmlrepdest%%kmlname%.lst"
-set kmltherr=0
+set kmlerr=0
 for %%i in ("%kmlrepscan%*.jp*g") do if not exist "%kmlrepdest%thumbs\%kmlname% - %%~ni.jpg" echo %%i >>"%kmlrepdest%%kmlname%.lst"
 if exist "%kmlrepdest%%kmlname%.lst" (
 if exist "%~dp0resize.py" (
 "%~dp0resize" "%kmlrepdest%%kmlname%.lst" "%kmlrepdest%thumbs\%kmlname%"
 ) else (
-"C:\Program Files (x86)\Irfanview\i_view32.exe" /filelist="%kmlrepdest%\%kmlname%.lst" /resize_long=150 /aspectratio /resample /ini="%~dp0" /convert="%kmlrepdest%thumbs\%kmlname% - $N.jpg"
-)
+"C:\Program Files (x86)\Irfanview\i_view32.exe" /filelist="%kmlrepdest%\%kmlname%.lst" /resize_long=150 /aspectratio /resample /ini="%~dp0" /convert="%kmlrepdest%thumbs\%kmlname% - $N.jpg")
 if !ERRORLEVEL! NEQ 0 (
-set kmltherr=1
-echo Erreur lors de la cr�ation des miniatures
-)
+set kmlerr=1
+echo Erreur lors de la cr�ation des miniatures)
 del "%kmlrepdest%%kmlname%.lst")
 set kmlfic="%kmlrepdest%%kmlname% - andro.kml"
-echo.
+echo. 2>"%kmlrepdest%%kmlname%.lst"
 echo Scan de "%kmlrepscan%" pour cr�ation de %kmlfic%
 for /F "delims=*" %%i in ('dir /b /o:n "%kmlrepscan%*.jp*g"') do echo %kmlrepscan%%%~nxi>>"%kmlrepdest%%kmlname%.lst"
 chcp 65001 >nul
+"C:\Program Files (x86)\ExifTool\exiftool.exe" -createdate -gpslatitude -gpslongitude -filename -d "%%d/%%m/%%Y %%H:%%M:%%S" -c "%%+.6f" -f -args -charset filename=Latin -charset EXIF=UTF8 -@ "%kmlrepdest%%kmlname%.lst" >"%kmlrepdest%%kmlname%.exif"
+if !ERRORLEVEL! NEQ 0 (
+set kmlerr=1
+echo Erreur lors de la récupération des métadonnées)
 echo ^<?xml version="1.0" encoding="UTF-8"?^>>%kmlfic%
 echo ^<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom"^>>>%kmlfic%
 echo ^<Folder^>>>%kmlfic%
 echo ^<name^>%kmlname%^</name^>>>%kmlfic%
-"C:\Program Files (x86)\ExifTool\exiftool.exe" -createdate -gpslatitude -gpslongitude -filename -d "%%d/%%m/%%Y %%H:%%M:%%S" -c "%%+.6f" -f -args -charset filename=Latin -charset EXIF=UTF8 -@ "%kmlrepdest%%kmlname%.lst" >"%kmlrepdest%%kmlname%.exif"
 echo ^<description^>^<^^![CDATA[Images du répertoire Carte SD/Picture/%kmlname%]]^>^</description^>>>%kmlfic%
 echo ^<Style id="placemark"^>>>%kmlfic%
 echo ^<IconStyle^>>>%kmlfic%
@@ -43,11 +44,19 @@ echo ^<BalloonStyle^>>>%kmlfic%
 echo ^<bgColor^>ff000000^</bgColor^>>>%kmlfic%
 echo ^<textColor^>ffaaaaaa^</textColor^>>>%kmlfic%
 echo ^<text^>>>%kmlfic%
-echo ^<^^![CDATA[^<div style="float:left;width:50%%;height:1.2em;overflow:hidden;color:white;"^>^<b^>%kmlname%^</b^>^</div^>^<div style="float:right;padding-top:0.2em;font-size:0.8em;color:#aaaaaa"^>$[date]^</div^>^<br/^>^<hr/^>>>%kmlfic%
+echo ^<^^![CDATA[>>%kmlfic%
+echo ^<div style="float:left;width:50%%;height:1.2em;overflow:hidden;color:white;"^>^<b^>%kmlname%^</b^>^</div^>>>%kmlfic%
+echo ^<div style="float:right;padding-top:0.2em;font-size:0.8em;color:#aaaaaa"^>$[date]^</div^>^<br/^>^<hr/^>>>%kmlfic%
 echo ^<b^>^<font color=#aaaaaa^>Lat: ^</b^>^<code^>$[lat]^</code^>^&nbsp;^&nbsp;^&nbsp;^<b^>Lon: ^</b^>^<code^>$[lon]^</code^>^<br/^>^<br/^>>>%kmlfic%
 echo ^<div style="width:96vw;height:64vh;"^>^<img src="$[url]" style="max-width:96vw;max-height:64vh;"/^>^</div^>^<br/^>>>%kmlfic%
 echo ^<b^>Nom: ^</b^>^<a href="$[url]"^>$[name]^</a^>^</font^>^<hr/^>]]^>>>%kmlfic%
 echo ^</text^>>>%kmlfic%
+echo ^</BalloonStyle^>>>%kmlfic%
+echo ^</Style^>>>%kmlfic%
+echo ^<Style^>>>%kmlfic%
+echo ^<BalloonStyle^>>>%kmlfic%
+echo ^<bgColor^>ff000000^</bgColor^>>>%kmlfic%
+echo ^<textColor^>ffaaaaaa^</textColor^>>>%kmlfic%
 echo ^</BalloonStyle^>>>%kmlfic%
 echo ^</Style^>>>%kmlfic%
 set kmlnbim=0
@@ -97,12 +106,11 @@ echo ^</kml^>>>%kmlfic%
 del "%kmlrepdest%%kmlname%.lst"
 del "%kmlrepdest%%kmlname%.exif"
 echo %kmlnbim% image^(s^) traitée^(s^)
-if not %kmlnbimsgps%%kmlnbimsdate%%kmltherr%==000 (
+if not %kmlnbimsgps%%kmlnbimsdate%%kmlerr%==000 (
 if not %kmlnbimsdate%==0 echo dont %kmlnbimsdate% image^(s^) intégrée^(s^) sans données de date de création
 if not %kmlnbimsgps%==0 echo dont %kmlnbimsgps% image^(s^) ignorée^(s^) ^(pas de données de géolocalisation^)
 echo.
-pause
-)
+pause)
 chcp 850 >nul
 endlocal
 echo on
